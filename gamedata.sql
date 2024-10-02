a30ea32e4fc81eaa50880fee20c5a0a09f4feded
Benjamin McLemore <51517288+Cheimonophile@users.noreply.github.com>
2024-10-02 18:54:44 -0400

    Fix endless turn bug ???

--- gamedata.sql
+++ gamedata.sql
@@ -3,22 +3,23 @@
 UPDATE GlobalParameters 
     SET Value = 0
     WHERE Name LIKE '%CITY_TERRAIN_REPLACE%';
+INSERT OR REPLACE INTO Building_YieldChanges
+    VALUES ('BUILDING_PALACE', 'YIELD_FOOD', 2);
+
 
 
 
 -- Delete all terrain yields
 DELETE FROM Terrain_YieldChanges;
-INSERT INTO Terrain_YieldChanges (TerrainType, YieldType, YieldChange)
-    SELECT TerrainType, 'YIELD_FOOD', 1
-    FROM Terrains
-    WHERE TerrainType LIKE '%GRASS%' OR 
-        TerrainType LIKE '%PLAINS%' OR
-        TerrainType LIKE '%COAST%';
+
+
+-- Floodplains give +1 food
 DELETE FROM Feature_YieldChanges
     WHERE FeatureType LIKE '%FLOODPLAINS%';
-
--- Weather events don't change tile yields
-DELETE FROM RandomEvent_Yields;
+INSERT INTO Feature_YieldChanges 
+    SELECT FeatureType, 'YIELD_FOOD', 1
+    FROM Features
+    WHERE FeatureType LIKE '%FLOODPLAINS%';
 
 
 -- Update Marshes
@@ -36,5 +37,6 @@ INSERT OR REPLACE INTO Improvement_ValidFeatures (ImprovementType, FeatureType)
 -- Allow fishing boats on all coast and lake tiles
 INSERT OR REPLACE INTO Improvement_ValidTerrains (ImprovementType, TerrainType)
     VALUES ('IMPROVEMENT_FISHING_BOATS', 'TERRAIN_COAST');
+s
 
 
